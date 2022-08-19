02e0f6486c28454b6d5a0dc32897fd8c06af7351
Andrew Wallace <wall0419@algonquinlive.com>
2022-08-19 08:10:55 -0400

    Selectors, templates, styles and how they connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    h2{
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'My First App';
+  name = 'Andrew';
 }
