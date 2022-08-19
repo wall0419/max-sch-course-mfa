29993d6243a5430bf9ba46baad7b5d5c5a49b6bb
Andrew Wallace <wall0419@algonquinlive.com>
2022-08-19 08:16:43 -0400

    Working with selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 //We can also specify the template directly in typescript via html selectors (useful for very small templates)
 @Component({
   //We can specify the selector to be an attribute rather than an element
-  selector: '[app-servers]',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>
